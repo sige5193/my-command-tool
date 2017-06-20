8c77f0d1c9e1826b0c06dbfa0562dc3a69fa6470
MicahelLuthor <micahelluthor@163.com>
2017-06-20 14:03:30 +0800

    增加Github抓取时等待时间。

--- Action/Github/PullOrganizations.php
+++ Action/Github/PullOrganizations.php
@@ -10,6 +10,8 @@ class PullOrganizations extends CommandActionAbstract {
      * @return void
      */
     protected function run( ) {
+        date_default_timezone_set('Asia/Shanghai');
+        
         $dbTmplPath = OhaCore::system()->getPath('Data/Github/data.tmpl.db');
         $dbPath = OhaCore::system()->getPath('Data/Github/data.db', '/');
         if ( !file_exists($dbPath) ) {
@@ -22,27 +24,44 @@ class PullOrganizations extends CommandActionAbstract {
         $cfg->set_connections(array('dev'=>"sqlite://{$dbPath}?urlencoded=true"));
         $cfg->set_default_connection('dev');
         
+        $mainConfig = OhaCore::system()->getConfig();
         $position = 0;
         do {
             $client = new \GuzzleHttp\Client(['base_uri' => 'https://api.github.com']);
             $response = $client->request('GET', '/organizations', [
-                'query' => ['since' => $position],
+                'query' => [
+                    'since' => $position, 
+                    'client_id'=>$mainConfig['Github']['ClientID'], 
+                    'client_secret'=>$mainConfig['Github']['ClientSecret']],
                 'verify'=>false,
-            ])->getBody();
-            $response = json_decode($response, true);
-            if ( empty($response) ) {
+            ]);
+            if ( $this->checkRateRemainsAndBreakLimit($response) ) {
+                # 如果到了限制并且已经突破，则需要重新请求。
+                continue;
+            }
+            
+            $responseJson = json_decode($response->getBody(), true);
+            if ( empty($responseJson) ) {
                 break;
             }
             
-            foreach ( $response as $item ) {
+            for ( $index=0; $index<count($responseJson); $index++ ) {
+                $item = $responseJson[$index];
                 if ( Organization::exists(array('id'=>$item['id'])) ) {
                     $this->info('%s %s [exists]', $item['id'], $item['login']);
                     continue;
                 }
                 
-                $detailResponse = $client->request('GET', "orgs/{$item['login']}", ['verify'=>false])->getBody();
-                $orgDetail = json_decode($detailResponse, true);
-                $this->info('%s %s %s', $orgDetail['id'], $orgDetail['login'], $orgDetail['name']);
+                $detailResponse = $client->request('GET', "orgs/{$item['login']}", ['verify'=>false]);
+                if ( $this->checkRateRemainsAndBreakLimit($detailResponse) ) {
+                    $index --;
+                    # 如果到了限制并且已经突破，则需要重新请求。
+                    continue;
+                }
+                
+                $orgDetail = json_decode($detailResponse->getBody(), true);
+                $name = isset($orgDetail['name']) ? $orgDetail['name'] : 'No-Name';
+                $this->info('%s %s %s', $orgDetail['id'], $orgDetail['login'], $name);
                 
                 $org = new Organization();
                 $org->set_attributes($orgDetail);
@@ -52,5 +71,27 @@ class PullOrganizations extends CommandActionAbstract {
         } while ( true );
         
         $this->info("Done Pulling : %d Orgs", Organization::count());
+    }
+    
+    /**
+     * @param unknown $response
+     * @return boolean
+     */
+    private function checkRateRemainsAndBreakLimit( $response ) {
+        $rateLimit = $response->getHeader('X-RateLimit-Limit');
+        $rateRemain = $response->getHeader('X-RateLimit-Remaining');
+        $rateResetTime = $response->getHeader('X-RateLimit-Reset');
+        if ( 10 < $rateRemain[0] ) {
+            return false;
+        }
+        
+        $waitSeconds = $rateResetTime[0] - time();
+        $this->info("Rate Limit Remains lower than 10.");
+        while ( $waitSeconds > 0 ) {
+            $this->info("You need to wait for {$waitSeconds} secs to back to {$rateLimit[0]}r/h.");
+            $waitSeconds --;
+            sleep(1);
+        }
+        return true;
     }
 }
--- Configuration/Main.php
+++ Configuration/Main.php
@@ -4,6 +4,10 @@ return array(
     'Input' => 'GB2312',
     'Output' => 'GB2312//IGNORE',
 ),
+'Github' => array(
+    'ClientID' => '1136c15a0aefd011d8b7',
+    'ClientSecret'=>'4592cdf9fa8ec2dc8b08cb34fb54e4a01c65b5ec',
+),
 'ShgtSiteAdmin' => array(
     'MenuItem' => json_decode(file_get_contents(__DIR__.'/shgt-site-admin-menu-item.json'), true),
     'DataBaseTableInfo' => array(
