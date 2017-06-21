bba696c378e0e76f1b4b9b32609f9670fba24f35
MicahelLuthor <micahelluthor@163.com>
2017-06-21 16:02:35 +0800

    [github/pullOrganizations]使用异步拉取组织信息。

--- Action/Github/PullOrganizations.php
+++ Action/Github/PullOrganizations.php
@@ -4,7 +4,14 @@ use Core\CommandActionAbstract;
 use Core\OhaCore;
 use Model\Github\Organization;
 use function GuzzleHttp\json_decode;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Pool;
 class PullOrganizations extends CommandActionAbstract {
+    /** 拉取信息时的偏移值 */
+    private $position = 0;
+    /** 已获组织数量 */
+    private $orgCounter = 0;
+    
     /**
      * 拉取Github的组织信息并且存储到本地数据库中。
      * @return void
@@ -22,19 +29,20 @@ class PullOrganizations extends CommandActionAbstract {
         $cfg->set_connections(array('dev'=>"sqlite://{$dbPath}?urlencoded=true"));
         $cfg->set_default_connection('dev');
         
+        $this->orgCounter = Organization::count(array('conditions' => '1=1'));
+        
         $mainConfig = OhaCore::system()->getConfig();
-        $position = 0;
         $lastOrg = Organization::find('first', array('order'=>'id DESC','limit'=>1));
         if ( null !== $lastOrg ) {
-            $position = $lastOrg->id;
+            $this->position = $lastOrg->id;
         }
         do {
             $client = new \GuzzleHttp\Client(['base_uri' => 'https://api.github.com']);
             $response = $client->request('GET', '/organizations', [
                 'query' => [
-                    'since' => $position, 
                     'client_id'=>$mainConfig['Github']['ClientID'], 
-                    'client_secret'=>$mainConfig['Github']['ClientSecret']],
+                    'client_secret'=>$mainConfig['Github']['ClientSecret'],
+                    'since' => $this->position,],
                 'verify'=>false,
             ]);
             if ( $this->checkRateRemainsAndBreakLimit($response) ) {
@@ -47,38 +55,47 @@ class PullOrganizations extends CommandActionAbstract {
                 break;
             }
             
-            for ( $index=0; $index<count($responseJson); $index++ ) {
-                $item = $responseJson[$index];
-                if ( Organization::exists(array('id'=>$item['id'])) ) {
-                    $this->info('%s %s [exists]', $item['id'], $item['login']);
-                    continue;
-                }
-                
-                $detailResponse = $client->request('GET', "orgs/{$item['login']}", [
-                    'query' => [
-                        'client_id'=>$mainConfig['Github']['ClientID'],
-                        'client_secret'=>$mainConfig['Github']['ClientSecret']],
-                    'verify'=>false
-                ]);
-                $rateMessage = '';
-                if ( $this->checkRateRemainsAndBreakLimit($detailResponse, $rateMessage) ) {
-                    $index --;
-                    # 如果到了限制并且已经突破，则需要重新请求。
-                    continue;
-                }
-                
-                $orgDetail = json_decode($detailResponse->getBody(), true);
-                $name = isset($orgDetail['name']) ? $orgDetail['name'] : 'No-Name';
-                $this->info('%s %s %s %s', $rateMessage, $orgDetail['id'], $orgDetail['login'], $name);
-                
-                $org = new Organization();
-                $org->set_attributes($orgDetail);
-                $org->save();
-                $position = $item['id'];
+            $requestJobs = array();
+            foreach ( $responseJson as $responseJsonItem ) {
+                $requestJobs[] = new Request('GET', "orgs/{$responseJsonItem['login']}");
             }
+            
+            $poolOption = array();
+            $poolOption['fulfilled'] = array($this,'onPullOrgDetailSuccessed');
+            $poolOption['rejected'] = array($this,'onPullOrgDetailFailed');
+            $poolOption['concurrency'] = count($responseJson);
+            $poolOption['options']['verify'] = false;
+            $poolOption['options']['query']['client_id'] = $mainConfig['Github']['ClientID'];
+            $poolOption['options']['query']['client_secret'] = $mainConfig['Github']['ClientSecret'];
+            //$poolOption['options']['proxy'] = 'tcp://localhost:8125';
+            $pool = new Pool($client, $requestJobs,$poolOption);
+            $promise = $pool->promise();
+            $promise->wait();
         } while ( true );
         
         $this->info("Done Pulling : %d Orgs", Organization::count());
+    }
+    
+    /** 获取组织详情信息成功时调用。 */
+    public function onPullOrgDetailSuccessed( $response,$index ) {
+        $rateMessage = '';
+        $this->checkRateRemainsAndBreakLimit($response, $rateMessage);
+        
+        $orgDetail = json_decode($response->getBody(), true);
+        $name = isset($orgDetail['name']) ? $orgDetail['name'] : 'No-Name';
+        $this->info('%s %s %s %s', $rateMessage, $orgDetail['id'], $orgDetail['login'], $name);
+        
+        $org = new Organization();
+        $org->set_attributes($orgDetail);
+        $org->save();
+        
+        $this->position = $orgDetail['id'];
+        $this->orgCounter ++;
+    }
+    
+    /** 获取组织信息失败时调用 */
+    public function onPullOrgDetailFailed($reason, $index) {
+        $this->info($reason);
     }
     
     /**
