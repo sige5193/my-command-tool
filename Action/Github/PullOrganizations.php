dca1432991bd48cd486a1983d0b9c8088e83c263
MicahelLuthor <micahelluthor@163.com>
2017-06-22 14:29:20 +0800

    github/pull-organizations增加代理支持，当一个资源用完时，使用代理继续请求。

--- Action/Github/PullOrganizations.php
+++ Action/Github/PullOrganizations.php
@@ -11,6 +11,17 @@ class PullOrganizations extends CommandActionAbstract {
     private $position = 0;
     /** 已获组织数量 */
     private $orgCounter = 0;
+    /** 是否已经达到限制需要更换请求者 */
+    private $isSwitchRequesterRequired = false;
+    /** 当前请求者信息 */
+    private $currentRequester = array(
+        'index' => null,
+        'Name' => null,
+        'ClientID' => null,
+        'ClientSecret' => null,
+        'Proxy' => null,
+        'Concurrency' => null,
+    );
     
     /**
      * 拉取Github的组织信息并且存储到本地数据库中。
@@ -30,26 +41,16 @@ class PullOrganizations extends CommandActionAbstract {
         $cfg->set_default_connection('dev');
         
         $this->orgCounter = Organization::count(array('conditions' => '1=1'));
-        
-        $mainConfig = OhaCore::system()->getConfig();
         $lastOrg = Organization::find('first', array('order'=>'id DESC','limit'=>1));
         if ( null !== $lastOrg ) {
             $this->position = $lastOrg->id;
         }
+        
+        $this->switchRequester();
         do {
+            $this->info("Query List offset={$this->position}");
             $client = new \GuzzleHttp\Client(['base_uri' => 'https://api.github.com']);
-            $response = $client->request('GET', '/organizations', [
-                'query' => [
-                    'client_id'=>$mainConfig['Github']['ClientID'], 
-                    'client_secret'=>$mainConfig['Github']['ClientSecret'],
-                    'since' => $this->position,],
-                'verify'=>false,
-            ]);
-            if ( $this->checkRateRemainsAndBreakLimit($response) ) {
-                # 如果到了限制并且已经突破，则需要重新请求。
-                continue;
-            }
-            
+            $response = $client->request('GET', '/organizations', $this->getRequestOption(array('since'=>$this->position)));
             $responseJson = json_decode($response->getBody(), true);
             if ( empty($responseJson) ) {
                 break;
@@ -64,13 +65,18 @@ class PullOrganizations extends CommandActionAbstract {
             $poolOption['fulfilled'] = array($this,'onPullOrgDetailSuccessed');
             $poolOption['rejected'] = array($this,'onPullOrgDetailFailed');
             $poolOption['concurrency'] = count($responseJson);
-            $poolOption['options']['verify'] = false;
-            $poolOption['options']['query']['client_id'] = $mainConfig['Github']['ClientID'];
-            $poolOption['options']['query']['client_secret'] = $mainConfig['Github']['ClientSecret'];
-            //$poolOption['options']['proxy'] = 'tcp://localhost:8125';
+            if ( null !== $this->currentRequester['Concurrency'] ) {
+                $poolOption['concurrency'] = $this->currentRequester['Concurrency'];
+            }
+            $poolOption['options'] = $this->getRequestOption();
             $pool = new Pool($client, $requestJobs,$poolOption);
             $promise = $pool->promise();
             $promise->wait();
+            
+            if ( $this->isSwitchRequesterRequired ) {
+                $this->position = $responseJson[0]['id'];
+                $this->switchRequester();
+            }
         } while ( true );
         
         $this->info("Done Pulling : %d Orgs", Organization::count());
@@ -79,11 +85,18 @@ class PullOrganizations extends CommandActionAbstract {
     /** 获取组织详情信息成功时调用。 */
     public function onPullOrgDetailSuccessed( $response,$index ) {
         $rateMessage = '';
-        $this->checkRateRemainsAndBreakLimit($response, $rateMessage);
+        if ( !$this->checkLimitRateRemains($response, $rateMessage) ) {
+            $this->isSwitchRequesterRequired = true;
+            return;
+        }
         
         $orgDetail = json_decode($response->getBody(), true);
         $name = isset($orgDetail['name']) ? $orgDetail['name'] : 'No-Name';
         $this->info('|%d| %s %s %s %s', $this->orgCounter, $rateMessage, $orgDetail['id'], $orgDetail['login'], $name);
+        
+        if ( Organization::exists(array('id'=>$orgDetail['id'])) ) {
+            return;
+        }
         
         $org = new Organization();
         $org->set_attributes($orgDetail);
@@ -100,29 +113,108 @@ class PullOrganizations extends CommandActionAbstract {
         $this->info($reason);
     }
     
+    /** 获取请求配置信息 */
+    private function getRequestOption($query=array()) {
+        $option = array();
+        $option['verify'] = false;
+        $option['query'] = $query;
+        $option['query']['client_id'] = $this->currentRequester['ClientID'];
+        $option['query']['client_secret'] = $this->currentRequester['ClientSecret'];
+        if ( null !== $this->currentRequester['Proxy'] ) {
+            $option['proxy'] = $this->currentRequester['Proxy'];
+        }
+        return $option;
+    }
+    
+    /** 更换请求者 */
+    private function switchRequester() {
+        $this->info("Switch requester : start.");
+        $mainConfig = OhaCore::system()->getConfig();
+        $githubApps = $mainConfig['Github']['AppInfos'];
+        
+        $activeIndex = $this->currentRequester['index'];
+        if ( null === $activeIndex ) {
+            $activeIndex = 0;
+        } else if ( $activeIndex+1 >= count($githubApps) ) {
+            $activeIndex = 0;
+        } else {
+            $activeIndex ++;
+        }
+        
+        $minWaitTime = null;
+        $minWaitTimeIndex = null;
+        for ( ; $activeIndex<count($githubApps); $activeIndex++ ) {
+            $this->currentRequester = array(
+                'index' => $activeIndex,
+                'Name' => $githubApps[$activeIndex]['Name'],
+                'ClientID' => $githubApps[$activeIndex]['ClientID'],
+                'ClientSecret' => $githubApps[$activeIndex]['ClientSecret'],
+                'Proxy' => $githubApps[$activeIndex]['Proxy'],
+                'Concurrency' => $githubApps[$activeIndex]['Concurrency'],
+            );
+            
+            try {
+                $client = new \GuzzleHttp\Client(['base_uri' => 'https://api.github.com']);
+                $response = $client->request('GET', 'orgs/github', $this->getRequestOption());
+            } catch ( \GuzzleHttp\Exception\ClientException $e ) {
+                $this->info("Switch requester : %s blocked.", $githubApps[$activeIndex]['Name']);
+                continue;
+            }
+
+            $rateMessage = null;
+            $waitTime = 0;
+            if ( $this->checkLimitRateRemains($response, $rateMessage, $waitTime) ) {
+                $this->info("Switch requester : %s available.", $githubApps[$activeIndex]['Name']);
+                $this->isSwitchRequesterRequired = false;
+                break;
+            }
+            
+            $this->info("Switch requester : %s remains %d secs.", $githubApps[$activeIndex]['Name'], $waitTime);
+            if ( null===$minWaitTime || $waitTime<$minWaitTime ){
+                $minWaitTime = $waitTime;
+                $minWaitTimeIndex = $activeIndex;
+            }
+        }
+        
+        if ( $this->isSwitchRequesterRequired ) {
+            $minWaitTime += 5;
+            while ( $minWaitTime > 0 ) {
+                $this->info("You need to wait for {$minWaitTime} secs to back to top limit.");
+                $minWaitTime --;
+                sleep(1);
+            }
+            
+            $this->currentRequester = array(
+                'index' => $minWaitTimeIndex,
+                'Name' => $githubApps[$minWaitTimeIndex]['Name'],
+                'ClientID' => $githubApps[$minWaitTimeIndex]['ClientID'],
+                'ClientSecret' => $githubApps[$minWaitTimeIndex]['ClientSecret'],
+                'Proxy' => $githubApps[$minWaitTimeIndex]['Proxy'],
+                'Concurrency' => $githubApps[$minWaitTimeIndex]['Concurrency'],
+            );
+        }
+    }
+    
     /**
      * @param unknown $response
      * @return boolean
      */
-    private function checkRateRemainsAndBreakLimit( $response, &$message=null ) {
+    private function checkLimitRateRemains( $response, &$message=null, &$rateResetSeconds=null ) {
         $rateLimit = $response->getHeader('X-RateLimit-Limit');
         $rateRemain = $response->getHeader('X-RateLimit-Remaining');
         $rateResetTime = $response->getHeader('X-RateLimit-Reset');
         if ( null !== $message ) {
-            $message = "[L:{$rateLimit[0]} R:{$rateRemain[0]}]";
-        }
-        
-        if ( 10 < $rateRemain[0] ) {
-            return false;
+            $message = "[U:{$this->currentRequester['Name']} L:{$rateLimit[0]} R:{$rateRemain[0]}]";
         }
         
         $waitSeconds = $rateResetTime[0] - time();
-        $this->info("Rate Limit Remains lower than 10.");
-        while ( $waitSeconds > 0 ) {
-            $this->info("You need to wait for {$waitSeconds} secs to back to {$rateLimit[0]}r/h.");
-            $waitSeconds --;
-            sleep(1);
+        if ( null !== $rateResetSeconds ) {
+            $rateResetSeconds = $waitSeconds;
         }
-        return true;
+        
+        if ( 40 > $rateRemain[0] ) {
+            return 0;
+        }
+        return $rateRemain[0];
     }
 }
--- Configuration/Main.php
+++ Configuration/Main.php
@@ -8,8 +8,22 @@ return array(
     'Output' => 'GB2312//IGNORE',
 ),
 'Github' => array(
-    'ClientID' => '1136c15a0aefd011d8b7',
-    'ClientSecret'=>'4592cdf9fa8ec2dc8b08cb34fb54e4a01c65b5ec',
+    'AppInfos' => array(
+        array(
+            'Name' => 'App2',
+            'ClientID' => '7edd98588f7b65c31d67',
+            'ClientSecret'=>'a360fabee3b6bfbf9ca0335f33bbacf2972a236e',
+            'Proxy' => 'https://122.114.197.131:16816',
+            'Concurrency' => 5,
+        ),
+        array(
+            'Name' => 'App1',
+            'ClientID' => '1136c15a0aefd011d8b7',
+            'ClientSecret'=>'4592cdf9fa8ec2dc8b08cb34fb54e4a01c65b5ec',
+            'Proxy' => null,
+            'Concurrency' => null,
+        ),
+    ),
 ),
 'ShgtSiteAdmin' => array(
     'MenuItem' => json_decode(file_get_contents(__DIR__.'/shgt-site-admin-menu-item.json'), true),
